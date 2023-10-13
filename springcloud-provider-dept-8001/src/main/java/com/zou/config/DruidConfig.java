8f59fee5835f21c86da6e470a7247fc82bc98543
LinChuan <zoulinchuan2004@163.com>
2023-10-13 11:57:03 +0800

    2023/10/13

--- springcloud-provider-dept-8001/src/main/java/com/zou/config/DruidConfig.java
+++ springcloud-provider-dept-8001/src/main/java/com/zou/config/DruidConfig.java
@@ -1,36 +1,36 @@
 package com.zou.config;
 
-//import com.alibaba.druid.pool.DruidDataSource;
-//import com.alibaba.druid.support.http.StatViewServlet;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.boot.web.servlet.ServletRegistrationBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//@Configuration
-//public class DruidConfig {
-//
-//    @Bean
-//    @ConfigurationProperties(prefix = "spring.datasource.druid")
-//    public DruidDataSource druidDataSource(){
-//        return new DruidDataSource();
-//    }
-//
-//    //后台监控
-//    @Bean
-//    public ServletRegistrationBean registrationBean(){
-//        ServletRegistrationBean bean=new ServletRegistrationBean(new StatViewServlet());
-//        //代表访问什么样的请求可以进入到druid监控控制台(如果有虚拟目录druid会自定帮我们加上)
-//        //访问就是: 域名:端口/项目虚拟目录/druid
-//        bean.addUrlMappings("/druid/*");
-//        Map<String,String> initParams=new HashMap<String,String>();
-//        initParams.put("loginUsername", "admin");//账户密码是固定的
-//        initParams.put("loginPassword", "123456");
-//        initParams.put("allow", "");//这个值为空就允许所有人访问
-//        bean.setInitParameters(initParams);
-//        return bean;
-//    }
-//}
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class DruidConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DruidDataSource druidDataSource(){
+        return new DruidDataSource();
+    }
+
+    //后台监控
+    @Bean
+    public ServletRegistrationBean registrationBean(){
+        ServletRegistrationBean bean=new ServletRegistrationBean(new StatViewServlet());
+        //代表访问什么样的请求可以进入到druid监控控制台(如果有虚拟目录druid会自定帮我们加上)
+        //访问就是: 域名:端口/项目虚拟目录/druid
+        bean.addUrlMappings("/druid/*");
+        Map<String,String> initParams=new HashMap<String,String>();
+        initParams.put("loginUsername", "admin");//账户密码是固定的
+        initParams.put("loginPassword", "123456");
+        initParams.put("allow", "");//这个值为空就允许所有人访问
+        bean.setInitParameters(initParams);
+        return bean;
+    }
+}
